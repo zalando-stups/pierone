3090636bfb34c2137d83fa8a24f4eee6ec1dd677
Tobias Sarnowski <tobias@sarnowski.io>
2015-04-07 21:50:45 +0200

    use postgresql 9.4 on travis

---
+++ coveralls.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+COVERALLS_URL='https://coveralls.io/api/v1/jobs'
+CLOVERAGE_VERSION='1.0.4' lein2 cloverage -o cov --coveralls
+curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
