4ea844e262f51b5fadbbd2e5a6d585c046adb1b1
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-06-17 16:16:30 +0200

    set fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/pierone "0.5.0-SNAPSHOT"
+(defproject org.zalando.stups/pierone "0.4.0-OPENSOURCE"
   :description "Pier One Docker Registry"
   :url "https://github.com/zalando-stups/pierone"
   :license {:name "Apache License"
