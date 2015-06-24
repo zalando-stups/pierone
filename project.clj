4d247c4ebd99a8d59c3954a0d8f6b021da96fcad
Henning Jacobs <henning@jacobs1.de>
2015-06-24 17:45:52 +0200

    #9 use org/zalando/swagger1st/0.15.0

--- project.clj
+++ project.clj
@@ -6,9 +6,10 @@
   :scm {:url "git@github.com:zalando-stups/pierone"}
   :min-lein-version "2.0.0"
 
-  :dependencies [[org.zalando.stups/friboo "0.23.0"]
+  :dependencies [[org.zalando.stups/friboo "0.23.0" :exclusions [io.sarnowski/swagger1st]]
                  [yesql "0.5.0-rc3"]
-                 [io.sarnowski/swagger1st "0.11.0-SNAPSHOT"]
+                 ; TODO: remove the following line as soon as new version of friboo is released
+                 [org.zalando/swagger1st "0.15.0"]
 
                  [org.apache.commons/commons-compress "1.9"]
                  [org.clojure/data.codec "0.1.0"]
