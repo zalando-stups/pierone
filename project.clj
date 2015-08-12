af4a28a628dbbc02351308ba66690defd9e0dfaa
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-08-12 11:00:23 +0200

    Version 0.16.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/pierone "0.16.0"
+(defproject org.zalando.stups/pierone "0.16.1-SNAPSHOT"
   :description "Pier One Docker Registry"
   :url "https://github.com/zalando-stups/pierone"
   :license {:name "Apache License"
