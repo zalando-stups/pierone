cfea99e21428cfc0904ffaed3a088b86e9eacc0d
Henning Jacobs <henning@jacobs1.de>
2015-09-16 12:54:14 +0200

    use latest Friboo with caching

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.zalando.stups/friboo "1.2.0"]
+                 [org.zalando.stups/friboo "1.3.0"]
                  [yesql "0.5.0"]
 
                  [org.apache.commons/commons-compress "1.10"]
