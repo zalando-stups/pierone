d3ac940eaf9b8cfd18383cacc65c8c0bab2a3011
Henning Jacobs <henning@jacobs1.de>
2015-09-16 12:56:52 +0200

    Version 1.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.zalando.stups/pierone "1.5.0"
+(defproject org.zalando.stups/pierone "1.6.0-SNAPSHOT"
   :description "Pier One Docker Registry"
   :url "https://github.com/zalando-stups/pierone"
   :license {:name "Apache License"
