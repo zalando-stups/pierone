e6d61645f724e370d29e4365a86ac04ae80bd4a9
Christian Lohmann <christian.lohmann@zalando.de>
2016-04-11 10:31:00 +0200

    #65 bumped version of cheshire to 5.6.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.zalando.stups/friboo "1.5.3"]
                  [yesql "0.5.1"]
-                 [cheshire "5.5.1-SNAPSHOT"]
+                 [cheshire "5.6.0"]
                  [digest "1.4.4"]
                  [org.apache.commons/commons-compress "1.10"]
                  [org.clojure/data.codec "0.1.0"]]
