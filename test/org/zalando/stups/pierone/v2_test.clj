7d94da51685c14b009ac83546ac2833f05ddf9af
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-04-12 18:35:32 +0200

    #65 clean up tests

--- test/org/zalando/stups/pierone/v2_test.clj
+++ test/org/zalando/stups/pierone/v2_test.clj
@@ -13,42 +13,9 @@
       (apply str "response of wrong status: " response))
   (:body response))
 
-(def json-pretty-printer (json/create-pretty-printer
-                            { :indentation                  3
-                              :object-field-value-separator ": "
-                              :indent-arrays?               true
-                              :indent-objects?              true}))
-
 (deftest v2-test
   (let [system (u/setup)
-        manifest-file (slurp "test/org/zalando/stups/pierone/manifest.json")
-        manifest-file-v2 (slurp "test/org/zalando/stups/pierone/manifestv2.json")
-        manifest (json/parse-string manifest-file true)
-        data (.getBytes "imgdata")
-        ; echo -n 'imgdata' | sha256sum
-        ;digest sha256:a5c741c7dea3a96944022b4b9a0b1480cfbeef5f4cc934850e8afacb48e18c5e
-        digest (-> manifest :fsLayers first :blobSum)
-        manifestv2 (-> (json/parse-string manifest-file-v2 true)
-                       (assoc-in [:layers 0 :digest] digest)
-                       (assoc-in [:config :digest] digest))
-        invalid-manifest (.getBytes "stuff")
-        ;manifest (str "{\"fsLayers\":[{\"blobSum\":\"" digest "\"}]}")
-        pretty-manifest (json/encode manifest {:pretty json-pretty-printer})
-        pretty-manifest-v2 (json/encode manifestv2 {:pretty json-pretty-printer})
-        manifest-bytes (.getBytes pretty-manifest)
-        manifest-v2-bytes (.getBytes pretty-manifest-v2)
-        manifest-invalid-version-bytes (-> manifestv2
-                                           (assoc :schemaVersion "3")
-                                           (json/encode  {:pretty json-pretty-printer})
-                                           (.getBytes))
-        ; manifest-double is simply a different manifest (we use the same FS layer twice, does not make sense, but works)
-        manifest-double (update-in manifest [:fsLayers] (fn [old] (vec (take 2 (repeat (first old))))))
-        pretty-manifest-double (json/encode manifest-double {:pretty { :indentation 3
-                                                           :object-field-value-separator ": "
-                                                           :indent-arrays? true
-                                                           :indent-objects? true}})
-        manifest-double-bytes (.getBytes pretty-manifest-double)]
-
+        data (.getBytes "imgdata")]
     (u/wipe-db system)
 
     ; v2 compatibility check
@@ -57,7 +24,7 @@
       (is (= 200 (:status result))))
 
     (expect 404
-            (client/head (u/v2-url "/myteam/myart/blobs/" digest)
+            (client/head (u/v2-url "/myteam/myart/blobs/" (:digest d/manifest-v1))
                          (u/http-opts)))
 
     (expect 202
@@ -81,10 +48,10 @@
     (expect 201
             ; correct "digest" parameter
             (client/put (u/v2-url "/myteam/myart/blobs/uploads/myuuid")
-                        (merge (u/http-opts) {:query-params {"digest" digest}})))
+                        (merge (u/http-opts) {:query-params {"digest" (:digest d/manifest-v1)}})))
 
     (expect 200
-            (client/head (u/v2-url "/myteam/myart/blobs/" digest)
+            (client/head (u/v2-url "/myteam/myart/blobs/" (:digest d/manifest-v1))
                          (u/http-opts)))
 
     (expect 404
@@ -92,18 +59,20 @@
                         (u/http-opts)))
 
     (expect 400
+            ; not json
             (client/put (u/v2-url "/myteam/myart/manifests/1.0")
-                        (u/http-opts (io/input-stream invalid-manifest))))
+                        (u/http-opts (io/input-stream (.getBytes "invalid manifest")))))
 
     (expect 400
+            ; invalid schema version
             (client/put (u/v2-url "/myteam/myart/manifests/1.0")
-                        (u/http-opts (io/input-stream manifest-invalid-version-bytes))))
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v4)))))
 
     (expect 201
             (client/put (u/v2-url "/myteam/myart/manifests/1.0")
-                        (u/http-opts (io/input-stream manifest-bytes))))
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v1)))))
 
-    (is (= pretty-manifest
+    (is (= (:pretty d/manifest-v1)
            (expect 200
             (client/get (u/v2-url "/myteam/myart/manifests/1.0")
                         (u/http-opts)))))
@@ -124,18 +93,18 @@
                         (u/http-opts)))
 
     (expect 404
-            (client/get (u/v1-url "/images/" digest "/json")
+            (client/get (u/v1-url "/images/" (:digest d/manifest-v1) "/json")
                         (u/http-opts)))
 
     ; check that *-SNAPSHOT tags are mutable
     (expect 201
             (client/put (u/v2-url "/myteam/myart/manifests/1.0-SNAPSHOT")
-                        (u/http-opts (io/input-stream manifest-bytes))))
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v1)))))
 
     ; works, no changes
     (expect 201
             (client/put (u/v2-url "/myteam/myart/manifests/1.0-SNAPSHOT")
-                        (u/http-opts (io/input-stream manifest-bytes))))
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v1)))))
 
     (let [response (client/get (u/v2-url "/myteam/myart/manifests/1.0-SNAPSHOT")
                                 (u/http-opts))]
@@ -145,15 +114,15 @@
     ; update, new manifest
     (expect 201
             (client/put (u/v2-url "/myteam/myart/manifests/1.0-SNAPSHOT")
-                        (u/http-opts (io/input-stream manifest-double-bytes))))
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v1-multilayer)))))
 
-    (is (= pretty-manifest-double
+    (is (= (:pretty d/manifest-v1-multilayer)
            (expect 200
             (client/get (u/v2-url "/myteam/myart/manifests/1.0-SNAPSHOT")
                         (u/http-opts)))))
 
     (expect 201 (client/put (u/v2-url "/myteam/myart/manifests/2.0-SNAPSHOT")
-                                (u/http-opts (io/input-stream manifest-v2-bytes))))
+                                (u/http-opts (io/input-stream (:bytes d/manifest-v2)))))
 
     (let [response (client/get (u/v2-url "/myteam/myart/manifests/2.0-SNAPSHOT")
                                 (u/http-opts))]
