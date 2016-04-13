72bf6b8067aece52a344c5cd6d61790fc7ebad2b
Christian Lohmann <christianl.proxy@gmail.com>
2016-04-13 14:14:19 +0200

    65 docker 1 10 (#71)
    * #65 accept application/vnd.docker.distribution.manifest.v2+json
    
    * add schema_version to put manifest
    
    * add dump from orignal registry as comment
    
    * check for "./scm-source.json" also
    
    * add patch file for db changes
    
    * added content-type header to get-manifest endpoint
    
    * 65 fixed docker push and pull for docker version 1.1x
    
    * #65 reverted db changes for schema_version field in tags table,
    
    * #65 added additional headers to get-manifest
    
    * #65 fixed tests
    
    * #65 extracted json pretty-print config to global var
    
    * #65 removed teststring
    
    * #65 bumped version of cheshire to 5.6.0
    
    * #65 replaced pretty-print-format-config map with a preconfigured pretty-printer object
    
    * #65 added missing bracket
    
    * #65 added errors object
    
    * #65 added json error message for manifest not found errors
    
    * 65 added returning BLOB_UNKNOWN error on get-blob and put-upload
    
    * #65 extracted function to generate error messages
    
    * #65 added check for manifest version. if version is not 1 or 2 then wilt throw a 400
    
    * #65 replced cond with condp, some formatting
    
    * #65 some formatting
    
    * #65 clean up tests
    
    * #65 include pierone api tests again, remove delete-test-data (just wipe all the time)
    
    * #65 general housekeeping :lipstick:
    
    * #65 fixed wrong delimiter placement and added test for duplicate tag

--- test/org/zalando/stups/pierone/v2_test.clj
+++ test/org/zalando/stups/pierone/v2_test.clj
@@ -72,6 +72,11 @@
             (client/put (u/v2-url "/myteam/myart/manifests/1.0")
                         (u/http-opts (io/input-stream (:bytes d/manifest-v1)))))
 
+    (expect 409
+            ; duplicate tag
+            (client/put (u/v2-url "/myteam/myart/manifests/1.0")
+                        (u/http-opts (io/input-stream (:bytes d/manifest-v2)))))
+
     (is (= (:pretty d/manifest-v1)
            (expect 200
             (client/get (u/v2-url "/myteam/myart/manifests/1.0")
